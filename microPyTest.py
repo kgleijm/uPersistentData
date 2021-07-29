01101285a5e1b4db70d4462d016b1ed0d6007aac
Kevin <kevingleijm@gmail.com>
2021-07-29 22:43:59 +0200

    added explanatory comments

--- microPyTest.py
+++ microPyTest.py
@@ -1,6 +1,7 @@
 
 import json
 
+# "abstract" class to encapsulate variables and manage persistence by keeping a record on file
 class Pdp:
 
     # static dict keeping track of all Pdp values's
@@ -10,26 +11,26 @@ class Pdp:
         self.load()
         self.publicName = publicName
         self.verify(self.getdefault())
-        self.getCommunicationRepresentation()
         if not self.set(value):
             print("Error, initial value of", publicName, " does not pass validation")
             self.set(self.getdefault())
 
+    # Overridable method where childClasses can verify their input data
     def verify(self, value):
         raise NotImplementedError
 
+    # Returns a default when no initial value is supplied
     def getdefault(self):
         raise NotImplementedError
 
-    def getCommunicationRepresentation(self):
-        raise NotImplementedError
-
+    # Static "private" method to sync dictionary of values with record on file
     @staticmethod
     def save():
         with open("persistentData.json", "w") as f:
             json.dump(Pdp.persistentData, f)
             print("Pdp.save() saved persistent data")
 
+    # Static "private" method to sync dictionary of values with record on file
     @staticmethod
     def load():
         try:
@@ -40,6 +41,7 @@ class Pdp:
             print("persistentData.json does not exist, creating file with Pdp.save()")
             Pdp.save()
 
+    # Static method that clears out all data
     @staticmethod
     def reset():
         Pdp.persistentData = dict()
@@ -50,6 +52,7 @@ class Pdp:
         except:
             print()
 
+    # Set value to input with forced Verification
     def set(self, value):
         if self.verify(value):
             Pdp.persistentData[self.publicName] = value
@@ -58,14 +61,43 @@ class Pdp:
             return True
         return False
 
+    # Get value
     def get(self):
         return Pdp.persistentData[self.publicName]
+
+
+class PdpString(Pdp):
+
+    def __init__(self, publicName, value, maxLength):
+        self.maxLength = maxLength
+        self.PdpType = "stringPdp"
+        super().__init__(publicName, value)
+
+    def getdefault(self):
+        return ""
+
+    def verify(self, value):
+        return isinstance(value, str) and len(value) <= self.maxLength
+
+
+
+
+class PdpConfig(Pdp):
+
+    def __init__(self, publicName, value):
+        super().__init__(publicName, value)
+        self.getCommunicationRepresentation()
+        self.configType = "percentConfig"
+
+    def getCommunicationRepresentation(self):
+        raise NotImplementedError
+
 
 class PdpPercent(Pdp):
 
     def __init__(self, publicName, value):
         super().__init__(publicName, value)
-        self.configType = "percentConfig"
+        self.PdpType = "percent"
 
     def getdefault(self):
         return 0
@@ -76,19 +108,8 @@ class PdpPercent(Pdp):
         except:
             return False
 
-class PdpString(Pdp):
-
-    def __init__(self, publicName, value, maxLength):
-        super().__init__(publicName, value)
-        self.maxLength = maxLength
-        self.configType = "stringConfig"
 
 
-    def getdefault(self):
-        return ""
-
-    def verify(self, value):
-        return isinstance(value, str) and len(value) <= self.maxLength
 
 
 PdpString.reset()
